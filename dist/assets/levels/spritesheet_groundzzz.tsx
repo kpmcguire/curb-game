bfd96dd40a0acad5341031442eee3736c975d6b2
Kevin McGuire <kevin@givegab.com>
2020-02-25 01:54:59 -0500

    add assets to dist

---
+++ dist/assets/levels/spritesheet_groundzzz.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="spritesheet_groundzzz" tilewidth="128" tileheight="128" tilecount="3" columns="3">
+ <image source="../images/spritesheet_groundzzz.png" width="384" height="128"/>
+</tileset>
