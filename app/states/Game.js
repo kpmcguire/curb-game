e4d45690ed336fe8523061a3b9457eb8bf932733
Kevin McGuire <kevin@givegab.com>
2020-03-11 00:15:26 -0400

    skip curb intro if in dev

--- app/states/Game.js
+++ app/states/Game.js
@@ -2,8 +2,14 @@ export default class Game extends Phaser.State {
 
   create() {
     this.game.score = 0;
+
+    if (IS_DEV) {
+      this.game.state.start("Level1_K");  
+    } else {
+      this.game.state.start("CurbIntro");
+    }
     // this.game.state.start("CurbIntro");
-    this.game.state.start("Level1_K");
+    // this.game.state.start("Level1_K");
     // this.game.state.start("Level2_K");
 
   }
